8bfd50bb2602db9fd8baa0b031c4a9fcc67d9a07
Dex Chen <dex.chen@ruckuswireless.com>
2019-05-12 12:21:51 +0800

    add support for Pixiecore api server
    https://github.com/danderson/netboot/tree/master/pixiecore

--- testIso9660/main.go
+++ testIso9660/main.go
@@ -2,12 +2,19 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strconv"
 
 	"github.com/dex/iso9660"
 )
+
+var port = flag.Int("port", 8080, "Port to linsten on")
 
 type isoFs struct {
 	fs *iso9660.FileSystem
@@ -18,6 +25,7 @@ func (iso *isoFs) Open(name string) (http.File, error) {
 }
 
 func main() {
+	flag.Parse()
 	argc := len(os.Args)
 
 	if argc < 2 {
@@ -29,6 +37,27 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(&isoFs{fs})))
+	http.Handle("/repo/", http.StripPrefix("/repo/", http.FileServer(&isoFs{fs})))
+	http.HandleFunc("/v1/boot/", api)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
 
+}
+
+func api(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Serving boot config for %s", filepath.Base(r.URL.Path))
+	resp := struct {
+		K string   `json:"kernel"`
+		I []string `json:"initrd"`
+		C string   `json:"cmdline"`
+	}{
+		K: "/repo/isolinux/vmlinuz",
+		I: []string{
+			"/repo/isolinux/initrd.img",
+		},
+		C: fmt.Sprintf("ip=dhcp inst.repo=http://%s/repo inst.ks=http://10.206.83.38:8000/install.ks", r.Host),
+	}
+
+	if err := json.NewEncoder(w).Encode(&resp); err != nil {
+		panic(err)
+	}
 }
